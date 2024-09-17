7358c4d6c4b66fb392a88342252373da7bd64363
Viktor Streltsov <streltsov95va@gmail.com>
2024-09-17 18:48:35 +0300

    commit 1

--- HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
+++ HexletJavalin/src/main/java/org/example/hexlet/HelloWorld.java
@@ -6,10 +6,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
+            // Включаем логгирование при разработке
             config.bundledPlugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
+        // Описываем, что будет происходить при GET запросе на адрес /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
         app.start(7070); // Стартуем веб-сервер
     }
 }
